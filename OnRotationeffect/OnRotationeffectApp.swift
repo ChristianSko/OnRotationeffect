1ec5c361b314661baf7923413be2a729b732306d
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-01 14:39:33 +0200

    Initial Commit

---
+++ OnRotationeffect/OnRotationeffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnRotationeffectApp.swift
+//  OnRotationeffect
+//
+//  Created by Skorobogatow, Christian on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct OnRotationeffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
